457b3ba898fd227d7590cceca5292902fc9b39f0
Dallas Hyde <39468324+hydedall@users.noreply.github.com>
2022-08-30 00:14:57 -0400

    tried to complete gave up for the night

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -1,11 +1,28 @@
+// deps and app object
+
 const express = require(`express`)
 const app = express()
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send(`
-    <h1>99 bottles of beer on the wall</h1>
-    <a href="localhost:4000/:numbers_of_bottles">Take one down, pass it around</a>`)
+    res.send(`99 bottles of beer on the wall
+    </br>
+    <a href="http://localhost:3000/98">Take one down</a>`);
+})
+// req = request res = response
+app.get('/:bottles', function (req,res){
+    if (req.params.bottles - 1 === 0){
+        res.send(`${req.params.bottles} bottles of beer on the wall!
+        </br>
+        <a href ="http://localhost:3000/">Go Again.</a>`);
+    } else {
+        res.send(`${req.params.bottles} bottles of beer on the wall!
+        </br>
+        <a href="http://localhost:3000/${req.params.bottles - 1}>Take one down</a>`)
+    }   //basically just lots of requesting and then responses to the requests
+        
 });
 
 app.listen(3000)
+
+//I know there is definitely an easier way to do this, and I just couldn't figure out how to complete it.
