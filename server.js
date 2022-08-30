939243edbf40f95e8f965133c50d9f145e51ca36
Dallas Hyde <39468324+hydedall@users.noreply.github.com>
2022-08-29 23:18:53 -0400

    Second step

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get('/greeting', function (req,res) {
+app.get('/greeting', function (req, res) {
     res.send("Hello, stranger");
 })
-app.get('/greeting/:name', function (req,res) {
+app.get('/greeting/:name', function (req, res) {
     res.send(`Hello, ${req.params.name}`);
+})
+
+app.get('/tip/:total/:tipPercentage', function (req, res) {
+    res.send(`How much you should tip is ${parseInt(req.params.total) * (parseInt(req.params.tipPercentage) / 100)}`);
 })
 
 app.listen(3000)
