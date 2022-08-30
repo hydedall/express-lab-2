c5493e5d657f6fbe891cf9ad80c90eb810c4be12
Dallas Hyde <39468324+hydedall@users.noreply.github.com>
2022-08-29 23:17:08 -0400

    first step done

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-// Importing Express
-const express = require('express');
-
-// Create App Object
+const express = require("express");
 const app = express();
-const port = 3000;
+
+app.get('/greeting', function (req,res) {
+    res.send("Hello, stranger");
+})
+app.get('/greeting/:name', function (req,res) {
+    res.send(`Hello, ${req.params.name}`);
+})
+
+app.listen(3000)
